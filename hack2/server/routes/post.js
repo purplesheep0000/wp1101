49dd8b00f63a4f22f8568eda4bbb9754982270cb
CHUAN-HSI WANG <a44281234@gmail.com>
2021-12-07 11:05:22 +0800

    eg

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import Post from '../models/post'
+import moment from 'moment'
+
+const router = express.Router()
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+
+// TODO 4-(1): create the 3rd API (/api/newPost)
+
+// TODO 5-(1): create the 4th API (/api/post)
+
+export default router
---
+++ hack2/server/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import postRoute from './routes/post'
+import mongoose from 'mongoose'
+import { dataInit } from './upload'
+
+require('dotenv').config()
+const app = express()
+
+app.use(express.json())
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+})
+
+const port = process.env.PORT || 4000
+const dboptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
+// TODO 1: connect to your mongodb here
+
+app.use('/api', postRoute)
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`)
+})
